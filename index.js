9ef10e9105ad03d276010195dcefa0e1a1ab5c86
jorgemongeaa-pixel <jorgemonge.aa@gmail.com>
2026-05-24 21:26:55 -0600

    Prueba final: Cambio de mensaje en la API

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.json({ 
         status: "online", 
-        mensaje: "¡Proyecto Final de Sistemas Operativos II funcionando!",
+        mensaje: "¡Proyecto Final de Sistemas Operativos II funcionando! PRUE",
         entorno: "Contenedor Docker en Ubuntu"
     });
 });

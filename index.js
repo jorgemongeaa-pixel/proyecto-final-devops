ebcf156364168ff7d7d4f1166842ba9a48910e5c
jorgemongeaa-pixel <jorgemonge.aa@gmail.com>
2026-05-30 14:10:37 -0600

    Mejora visual: Añadido dashboard de monitoreo moderno

--- index.js
+++ index.js
@@ -8,4 +8,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
     console.log(`Aplicación DevOps corriendo en el puerto ${PORT}`);
+});const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+// Servir la carpeta estática "public" con el diseño HTML
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+    console.log(`Aplicación DevOps corriendo en el puerto ${PORT}`);
 });
